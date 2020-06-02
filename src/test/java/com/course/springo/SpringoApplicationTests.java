9f5e37d4279146010ff8d377c057ea123aa44887
Marcus Oliveira <marcus.oliveira@m4u.com.br>
2020-06-01 22:00:29 -0300

    first commit

---
+++ src/test/java/com/course/springo/SpringoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.springo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
