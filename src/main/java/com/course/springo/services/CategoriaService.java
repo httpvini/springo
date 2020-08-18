14d3f19479be7c903cb76968aa6a1440042f32ab
Marcus Oliveira <marcus.oliveira@m4u.com.br>
2020-08-18 00:04:18 -0300

    Adiciona Exception caso Categoria não seja encontrada no banco.
    A exception está sendo tratada por um ControllerAdvice / ExceptionHandler na camada web.

--- src/main/java/com/course/springo/services/CategoriaService.java
+++ src/main/java/com/course/springo/services/CategoriaService.java
@@ -2,8 +2,11 @@ package com.course.springo.services;
 
 import com.course.springo.domain.Categoria;
 import com.course.springo.repositories.CategoriaRepository;
+import com.course.springo.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
@@ -11,9 +14,10 @@ public class CategoriaService {
     @Autowired
     private CategoriaRepository categoriaRepository;
 
-    public Categoria buscar(Integer id){
-        Categoria categoria = categoriaRepository.findById(id).get();
-        return categoria;
+    public Categoria buscar(Integer id) {
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        return categoria.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: "+ id +", Tipo: "+Categoria.class.getName()));
     }
 
 }
