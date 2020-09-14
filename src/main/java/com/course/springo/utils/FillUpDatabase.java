440f3777166c8cb241667dc7eea8584947bcf70f
Marcus Oliveira <marcus.oliveira@m4u.com.br>
2020-09-13 22:26:33 -0300

    Ajusta classe de inicialização de dados no banco h2.

--- src/main/java/com/course/springo/utils/FillUpDatabase.java
+++ src/main/java/com/course/springo/utils/FillUpDatabase.java
@@ -1,13 +1,19 @@
 package com.course.springo.utils;
 
 import com.course.springo.domain.Categoria;
+import com.course.springo.domain.Cidade;
+import com.course.springo.domain.Estado;
 import com.course.springo.domain.Produto;
 import com.course.springo.repositories.CategoriaRepository;
+import com.course.springo.repositories.CidadeRepository;
+import com.course.springo.repositories.EstadoRepository;
 import com.course.springo.repositories.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class FillUpDatabase {
@@ -18,38 +24,69 @@ public class FillUpDatabase {
     @Autowired
     private ProdutoRepository produtoRepository;
 
+    @Autowired
+    private EstadoRepository estadoRepository;
 
-    private Categoria categoriaInformatica = new Categoria(null, "Informatica");
-    private Categoria categoriaEscritorio = new Categoria(null, "Escritorio");
+    @Autowired
+    private CidadeRepository cidadeRepository;
 
-    private Produto computador = new Produto(null, "Computador", 900.00);
-    private Produto impressora = new Produto(null, "Impressora", 200.00);
-    private Produto mouse = new Produto(null, "Mouse", 100.00);
+    List<Categoria> categorias = new ArrayList<>(Arrays.asList(
+            new Categoria(null, "Informatica"),
+            new Categoria(null, "Escritorio")
+    ));
+
+    List<Produto> produtos = new ArrayList<>(Arrays.asList(
+            new Produto(null, "Computador", 900.00),
+            new Produto(null, "Impressora", 200.00),
+            new Produto(null, "Mouse", 100.00)
+    ));
+
+    List<Estado> estados = new ArrayList<>(Arrays.asList(
+            new Estado(null, "Minas Gerais"),
+            new Estado(null, "São Paulo")
+    ));
+
+    List<Cidade> cidades = new ArrayList<>(Arrays.asList(
+            new Cidade(null, "Uberlândia", estados.get(0)),
+            new Cidade(null, "São Paulo", estados.get(1)),
+            new Cidade(null, "Campinas", estados.get(1))
+    ));
 
     public void fillUp() {
         associaProdutocategoria();
-        categoriaRepository.saveAll(Arrays.asList(categoriaEscritorio, categoriaInformatica));
-        produtoRepository.saveAll(Arrays.asList(computador, impressora, mouse));
+        categoriaRepository.saveAll(categorias);
+        produtoRepository.saveAll(produtos);
+        estadoRepository.saveAll(estados);
+        cidadeRepository.saveAll(cidades);
     }
 
     private void associaProdutocategoria() {
-        categoriaInformatica.getProdutos().addAll(
-                Arrays.asList(
-                        computador,
-                        impressora,
-                        mouse
-                )
-        );
+
+        Categoria categoriaInformatica = categorias.get(0);
+        Categoria categoriaEscritorio = categorias.get(1);
+
+        Produto computador = produtos.get(0);
+        Produto impressora = produtos.get(1);
+        Produto mouse = produtos.get(2);
+
+        Estado minasGerais = estados.get(0);
+        Estado saoPaulo = estados.get(1);
+
+        Cidade uberlandia = cidades.get(0);
+        Cidade saoPauloC = cidades.get(1);
+        Cidade campinas = cidades.get(2);
+
+        categoriaInformatica.getProdutos().addAll(produtos);
         categoriaEscritorio.getProdutos().add(impressora);
 
         computador.getCategorias().add(categoriaInformatica);
-        impressora.getCategorias()
-                .addAll(
-                        Arrays.asList(
-                                categoriaInformatica,
-                                categoriaEscritorio
-                        )
-                );
+        impressora.getCategorias().addAll(Arrays.asList(categoriaInformatica, categoriaEscritorio));
         mouse.getCategorias().add(categoriaInformatica);
+
+        minasGerais.getCidades().add(uberlandia);
+        saoPaulo.getCidades().addAll(Arrays.asList(saoPauloC, campinas));
+
     }
+
+
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+-- INSERT INTO PRODUTO (nome, preco) VALUES
+--   ('Computador', 900.00),
+--   ('Impressora', 200.00);
